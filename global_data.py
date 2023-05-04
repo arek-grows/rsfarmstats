2e4c294c84d56a3627ad6e0271d7ebeb5d63101c
Arek T <arkadiusztruszkowski94@gmail.com>
2023-05-04 02:08:26 -0400

    going

--- global_data.py
+++ global_data.py
@@ -2,8 +2,11 @@ from os import getcwd
 
 spreadsheet_name = "Herb Harvests.xlsx"
 spreadsheet_path = f"{getcwd()}\\{spreadsheet_name}"
-herbsheet_names = ["Harvest Input", "Stats", "Parsed Data (don't touch)"]
+# todo: make a plot sheet?
+herbsheet_names = ["Harvest Input", "Stats"]
 herb_names = [
     "Torstol", "Dwarf Weed", "Lantadyme", "Cadantine", "Snapdragon", "Kwuarm", "Avantoe", "Irit", "Toadflax", "Ranarr",
     "Harralander", "Tarromin", "Marrentill", "Guam"
 ]
+stats_name_list = ["# of Harvests", "Total Harvested", "Lowest Yield", "Highest Yield", "Death Chance",
+                   "Median Yield", "Average Yield", "Yield per Seed", "harvest_yields"]
--- main.py
+++ main.py
@@ -35,12 +35,14 @@ class HerbTable:
         self.total_harvested = None
         self.average_yield = None
         self.median_yield = None
+        self.yield_per_seed = None
 
     def calc_class_attrs(self):
         self.death_rate = self.deaths / self.nr_harvests
         self.total_harvested = sum(self.harvest_yields)
         self.average_yield = self.total_harvested / len(self.harvest_yields)
         self.median_yield = median(self.harvest_yields)
+        self.yield_per_seed = self.average_yield * 1.1
 
     def add_yields(self, yields: list):
         for yy in yields:
@@ -66,7 +68,7 @@ class HerbTable:
         Death Chance:    {self.death_rate * 100}%
         Median Yield:    {self.median_yield}
         Average Yield:   {self.average_yield}
-        Yield per Seed:  {self.average_yield * 1.1}
+        Yield per Seed:  {self.yield_per_seed}
 """)
         return True
 
@@ -90,7 +92,7 @@ class HerbTable:
 
 
 # todo: ? good i think
-def calc_data_to_herb_table(herb_object: HerbTable, yield_list: list):
+def calc_cumulative_data_to_yields(yield_list: list):
     """transforms cumulative data into yield per allotment harvest"""
     first_yield = yield_list[0]
     real_yields = [first_yield]
@@ -98,7 +100,46 @@ def calc_data_to_herb_table(herb_object: HerbTable, yield_list: list):
     for yy in yield_list[1:]:
         real_yields.append(yy - total_yield)
         total_yield = yy
-    herb_object.add_yields(real_yields)
+    return real_yields
+
+# todo: figure out how im doing this
+def update_herb_objects_through_harvest_sheet(sheet, herb_objs_dict: dict, herb_row_idxs: list):
+    # for hridx in herb_row_idxs:
+    #     current_herb_name = sts_sheet.cell(row=1, column=hridx).value
+    #     current_row = hridx + 1
+    #
+    #     while True:
+    #         if not sts_sheet.cell(row=current_row, column=hridx).value:
+    #             break
+    #         else:
+    #             for cc in range(hridx, hridx + 5):
+    #                 if sts_sheet.cell(row=current_row, )
+    #
+    #                 pass
+    #         current_row += 1
+    #     pass
+    return [{"Herb": [[23, 4], [34, 2]]}, {}]
+
+
+def convert_HerbTables_to_stats_sheet(herb_objs_dict: dict, sheet):
+    return
+
+
+def convert_stats_sheet_to_HerbTables(sts_sheet, herb_objs_dict: dict):
+    for rr in range(2, len(gd.herb_names) + 2):
+        herb_name = sts_sheet.cell(row=rr, column=1).value
+        if not sts_sheet.cell(row=rr, column=2).value:
+            continue
+        herb_objs_dict[herb_name].nr_harvests = sts_sheet.cell(row=rr, column=2).value
+        herb_objs_dict[herb_name].total_harvested = sts_sheet.cell(row=rr, column=3).value
+        herb_objs_dict[herb_name].lowest_yield = sts_sheet.cell(row=rr, column=4).value
+        herb_objs_dict[herb_name].highest_yield = sts_sheet.cell(row=rr, column=5).value
+        herb_objs_dict[herb_name].death_rate = sts_sheet.cell(row=rr, column=6).value
+        herb_objs_dict[herb_name].median_yield = sts_sheet.cell(row=rr, column=7).value
+        herb_objs_dict[herb_name].average_yield = sts_sheet.cell(row=rr, column=8).value
+        herb_objs_dict[herb_name].yield_per_seed = sts_sheet.cell(row=rr, column=9).value
+        herb_objs_dict[herb_name].harvest_yields = sts_sheet.cell(row=rr, column=10).value
+    return herb_objs_dict
 
 
 if __name__ == '__main__':
@@ -118,24 +159,27 @@ if __name__ == '__main__':
 
     input_sheet = herb_book[gd.herbsheet_names[0]]
     stats_sheet = herb_book[gd.herbsheet_names[1]]
-    parse_sheet = herb_book[gd.herbsheet_names[2]]
+
     # create default objects or load objects from spreadsheet using pickling
-    herb_objects = []
-    if not parse_sheet['B1'].value:
-        print("cell empty.")
-    else:
-        print("cell note empty")
+    #todo: working now - how to update objects if the class is updated?
+    herb_idx_locs_in_first_sheet = list(range(2, 3 + ((len(gd.herb_names) - 1) * 6), 6))
+    herb_objects = {herb_name: HerbTable(herb_name) for herb_name in gd.herb_names}
+    herb_objects = convert_stats_sheet_to_HerbTables(stats_sheet, herb_objects)
     while True:
         print("[add] new data from spreadsheet, show [stats], [exit]")
         match input().lower():
 
+            # todo: added data from spreadsheet should turn green
+            # todo: save after done
             case "add":
+
                 pass
 
             case "stats":
                 pass
 
             case "exit":
+                print("Goodbye!")
                 exit()
 
             case _:
