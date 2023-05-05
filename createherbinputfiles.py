d3b6195555129beccb8eaccb70f7035e06b94d89
Arek T <arkadiusztruszkowski94@gmail.com>
2023-05-05 00:49:58 -0400

    v1.0

--- createherbinputfiles.py
+++
@@ -1,9 +0,0 @@
-from main import database_file_name
-import pickle
-
-with open(database_file_name, "rb") as herb_file:
-    herb_objects = pickle.load(herb_file)
-
-for hh in herb_objects:
-    new_file_name = f"{hh.name} Input.txt"
-    open(new_file_name, "x")
